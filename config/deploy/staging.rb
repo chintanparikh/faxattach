8e779003ec9d5605c850e9fcd072f03e7cb1f9cb
chintanparikh <chintanparikh@gatech.edu>
2013-07-01 15:29:41 -0400

    Staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,3 @@ server 'faxattach.staging.myaidin.com', :app, :web, :primary => true
 set :rails_env, :staging
 set :scm, :git
 set :repository, 'git@github.com:chintanparikh/faxattach.git'
-set :deploy_via, :remote_cache
