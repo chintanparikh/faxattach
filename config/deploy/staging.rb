ef63592ebf2782fad09c3d2349362045356516ec
chintanparikh <chintanparikh@gatech.edu>
2013-07-03 12:02:23 -0400

    Unicorn

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server 'faxattach.staging.myaidin.com', :app, :web, :primary => true
+server 'faxattach-staging-new', :app, :web, :primary => true
 set :rails_env, :staging
 set :scm, :git
 set :repository, 'git@github.com:chintanparikh/faxattach.git'
